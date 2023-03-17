c87f6f04b9a8bc2c2cb33d2d504029884996688a
David N. Cortez Ayala <david.cortes.ayala@hotmail.com>
2023-03-17 14:02:27 -0600

    🦺 Work ready

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import '@styles/styles.scss';
 import Home from '@components/Home';
 import About from '@components/About';
+import Work from '@components/Work';
+import Testimonial from '@components/Testimonial';
+import Contact from '@components/Contact';
+import Footer from '@components/Footer';
 
 const App = () => {
   return (
     <div className='App'>
       <Home />
       <About />
+      <Work /> 
+      <Testimonial /> /*En espera */
+      <Contact /> /*En espera */
+      <Footer /> /*En espera */
     </div>
   )
 }
